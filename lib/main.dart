b724e022a4bed1f319dff2f7327d45d5010e6870
Jamilur Rahman <jamilxt@gmail.com>
2019-03-27 22:19:39 +0600

    extract the arguments inside an onGenerateRoute function provided to the MaterialApp constructor

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,23 @@ class MyApp extends StatelessWidget {
       // Provide a function to handle named routes. Use this function to
       // identify the named route being pushed and create the correct
       // Screen.
-/*
       onGenerateRoute: (settings) {
         // If you push the PassArguments route
-        if(settings.name == PassAr)
+        if (settings.name == PassArgumentsScreen.routeName) {
+          // Cast the arguments to the correct type: ScreenArguments.
+          final ScreenArguments args = settings.arguments;
+
+          // Then, extract the required data from the arguments and
+          // pass the data to the correct screen.
+          return MaterialPageRoute(builder: (context) {
+            return PassArgumentsScreen(
+              title: args.title,
+              message: args.message,
+            );
+          });
+        }
       },
-*/
+
       title: 'Navigate with Arguments',
       home: HomeScreen(),
     );
@@ -55,6 +66,22 @@ class HomeScreen extends StatelessWidget {
                   ),
                 );
               },
+            ),
+            // A button that navigates to a named route. For this route, extract
+            // the arguments in the onGenerateRoute function and pass them
+            // to the screen.
+            RaisedButton(
+              child: Text('Navigate to a named that accepts arguments'),
+              onPressed: () {
+                // When the user taps the button, navigate to a named route
+                // and provide the arguments as an optional parameter.
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments('Accepts Arguments Screen',
+                      'This message is extracted in the onGenerateRoute Function'),
+                );
+              },
             )
           ],
         ),
@@ -88,6 +115,37 @@ class ExtractArgumentsScreen extends StatelessWidget {
       ),
       body: Center(
         child: Text(args.message),
+      ),
+    );
+  }
+}
+
+// A Widget that accepts the necessary arguments via the constructor.
+class PassArgumentsScreen extends StatelessWidget {
+  static const routeName = 'passArguments';
+
+  final String title;
+  final String message;
+
+  // This Widget accepts the arguments as constructor parameters. It does not
+  // extract the arguments from the ModalRoute.
+  //
+  // The arguments are extracted by the onGenerateRoute function provided to the
+  // MaterialApp widget.
+  const PassArgumentsScreen({
+    Key key,
+    @required this.title,
+    @required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(message),
       ),
     );
   }
