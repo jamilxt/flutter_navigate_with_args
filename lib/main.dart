8050984585b6661ca95be3a3ff28ef0ee23e2468
Jamilur Rahman <jamilxt@gmail.com>
2019-03-27 21:59:14 +0600

    extract the arguments using the ModalRoute.of method

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Provide a function to handle named routes. Use this function to
+      // identify the named route being pushed and create the correct
+      // Screen.
+/*
+      onGenerateRoute: (settings) {
+        // If you push the PassArguments route
+        if(settings.name == PassAr)
+      },
+*/
+      title: 'Navigate with Arguments',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // A button that navigates to a named route that. The named route
+            // extracts the arguments by itself.
+            RaisedButton(
+              child: Text('Navigate to screen that extracts arguments'),
+              onPressed: () {
+                // When the user taps the button, navigate to the specific route
+                // and provide the arguments as part of the RouteSettings.
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ExtractArgumentsScreen(),
+                    // Pass the arguments as part of the RouteSettings. The
+                    // ExtractArgumentScreen reads the arguments from these
+                    // settings.
+                    settings: RouteSettings(
+                      arguments: ScreenArguments(
+                        'Extract Arguments Screen',
+                        'This message is extracted in the build method.',
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// You can pass any object to the arguments parameter. In this example, create a
+// class that contains both a customizable title and message.
+class ScreenArguments {
+  final String title;
+  final String message;
+
+  ScreenArguments(this.title, this.message);
+}
+
+// A Widget that extracts the necessary arguments from the ModalRoute.
+class ExtractArgumentsScreen extends StatelessWidget {
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute settings and cast
+    // them as ScreenArguments.
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+      ),
+      body: Center(
+        child: Text(args.message),
+      ),
+    );
+  }
+}
